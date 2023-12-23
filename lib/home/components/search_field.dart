cec0e96fa05e3c4b179e5d58dd51fb3a396f5689
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-12-23 21:46:22 +0800

    update: changed border color

--- lib/home/components/search_field.dart
+++ lib/home/components/search_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habhab/core/resources/app_colors.dart';
 
 class SearchField extends StatelessWidget {
   const SearchField({super.key});
@@ -10,6 +11,16 @@ class SearchField extends StatelessWidget {
           prefixIcon: Icon(Icons.search),
           hintText: "Search",
           border: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppColors.grey,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppColors.grey,
+            ), // Set the border color when the field is focused
+          ),
           contentPadding: EdgeInsets.all(8)),
     );
   }
