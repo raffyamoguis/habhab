9984bdc6b6eb4c453e6459898e56687eec052394
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-12-23 21:42:04 +0800

    update: font

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:habhab/core/resources/app_colors.dart';
 import 'package:habhab/core/resources/app_strings.dart';
 import 'package:habhab/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: AppStrings.appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        fontFamily: GoogleFonts.ibmPlexSans().fontFamily,
         useMaterial3: true,
       ),
       home: const Home(),
